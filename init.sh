a37f5857c2f4eff2a8693a77c0aa6c6c851d975a
diego <diegodaddy@icloud.com>
2022-08-29 19:25:56 +0800

    -

--- init.sh
+++ init.sh
@@ -6,8 +6,6 @@
 # Download conda and install.
 # Paste ~/.ssh/id_rsa.pub to github ssh config
 
-
-
 #update
 sudo apt update
 sudo apt upgrade
@@ -16,11 +14,10 @@ sudo apt upgrade
 sudo apt -y zsh
 chsh -s $(which zsh)
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
-cd ~
-git clone https://github.com/caikaidi/wsl.git
-rm .zshrc
-cp wsl/.zshrc .zshrc
-source ~/.zshrc
+git clone https://github.com/caikaidi/wsl.git ~/
+mv ~/.zshrc ~/.zshrc.backup
+cp ~/wsl/.zshrc ~/.zshrc
+# source ~/.zshrc
 
 #python
 ##use conda
@@ -29,20 +26,16 @@ source ~/.zshrc
 sudo apt install neovim
 sudo apt install nodejs
 #sudo apt install yum
-cd ~
-mkdir .vim
+mkdir ~/.vim
 cp ~/wsl/vimrc ~/.vim/vimrc
-mkdir .config/nvim
+mkdir ~/.config/nvim
 cp ~/wsl/init.vim ~/.config/nvim/init.vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 #git
 ssh-keygen -t rsa -b 4096 -C "diegodaddy@icloud.com"
-cat ~/.ssh/id_rsa.pub
+#cat ~/.ssh/id_rsa.pub
 git config --global user.name "diego"
 git config --global user.email "diegodaddy@icloud.com"
 git config --global core.editor vim
-
-#GUI
-#sudo apt install xfce4
